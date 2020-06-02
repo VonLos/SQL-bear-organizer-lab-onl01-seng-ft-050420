dd7e7e7baa32f1a01a93c2b5e73d849d6114046b
rylandgrounds <rylandgrounds@gmail.com>
2020-06-02 11:22:39 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 999, "male", "black", "evil", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 333, "female", "brown", "chill", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 222, "male", "brown", "hangry", false);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 111, "male", "brown", "uptight", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 888, "female", "white", "XD", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 19999, "male", "green", "grumpy", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 58, "female", "red", "sassy", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 444, "female", "gigabrown", "kudere", true);
